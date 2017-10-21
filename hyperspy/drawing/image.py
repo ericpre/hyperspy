Fix issue with navigation_plot when toggling units/px.

hyperspy/drawing/image.py
@@ -159,49 +159,20 @@ class ImagePlot(BlittedFigure):
 
     def configure(self):
         # Calibrate the axes of the navigator image
-        self._compute_extent()
+        self._calculate_extent()
         self._calculate_aspect()
 
-    def _compute_extent(self):
+    def _calculate_extent(self):
         xaxis = self.xaxis
         yaxis = self.yaxis
         if self._calibrated:
-            self._extent = (xaxis.low_index, xaxis.high_index,
-                            yaxis.high_index, yaxis.low_index)
-        else:
             self._extent = (xaxis.axis[0] - xaxis.scale / 2.,
                             xaxis.axis[-1] + xaxis.scale / 2.,
                             yaxis.axis[-1] + yaxis.scale / 2.,
                             yaxis.axis[0] - yaxis.scale / 2.)
-
-    def _set_axis_labels(self):
-        def units_to_display(units):
-            return 'px' if self._calibrated else units
-
-        xaxis = self.xaxis
-        yaxis = self.yaxis
-
-        self._xlabel = '%s' % str(xaxis)
-        if xaxis.units is not Undefined:
-            self._xlabel += ' (%s)' % units_to_display(xaxis.units)
-        self._ylabel = '%s' % str(yaxis)
-        if yaxis.units is not Undefined:
-            self._ylabel += ' (%s)' % units_to_display(yaxis.units)
-
-        if (xaxis.units == yaxis.units) and (xaxis.scale == yaxis.scale):
-            self._auto_scalebar = True
-            self._auto_axes_ticks = False
-            self.pixel_units = units_to_display(xaxis.units)
         else:
-            self._auto_scalebar = False
-            self._auto_axes_ticks = True
-
-        if self.axes_ticks is False:
-            self.ax.set_xticks([])
-            self.ax.set_yticks([])
-
-        self.ax.set_xlabel(self._xlabel)
-        self.ax.set_ylabel(self._ylabel)
+            self._extent = (xaxis.low_index - 0.5, xaxis.high_index + 0.5,
+                            yaxis.high_index + 0.5, yaxis.low_index - 0.5)
 
     def _calculate_aspect(self):
         xaxis = self.xaxis
@@ -218,7 +189,38 @@ class ImagePlot(BlittedFigure):
                 factor = min_asp ** -1 * xaxis.size / yaxis.size
                 self._auto_scalebar = False
                 self._auto_axes_ticks = True
-        self._aspect = np.abs(factor * xaxis.scale / yaxis.scale)
+        if self._calibrated:
+            self._aspect = np.abs(factor * xaxis.scale / yaxis.scale)
+        else:
+            self._aspect = np.abs((self._extent[1] - self._extent[0]) / (
+                self._extent[3] - self._extent[2]))
+
+    def _set_axis_labels(self):
+        xaxis = self.xaxis
+        yaxis = self.yaxis
+
+        self._xlabel = '%s' % str(xaxis)
+        if xaxis.units is not Undefined and self._calibrated:
+            self._xlabel += ' (%s)' % xaxis.units
+        self._ylabel = '%s' % str(yaxis)
+        if yaxis.units is not Undefined and self._calibrated:
+            self._ylabel += ' (%s)' % yaxis.units
+
+        if (xaxis.units == yaxis.units) and (xaxis.scale == yaxis.scale):
+            self._auto_scalebar = True
+            self._auto_axes_ticks = False
+            if self._calibrated:
+                self.pixel_units = xaxis.units
+        else:
+            self._auto_scalebar = False
+            self._auto_axes_ticks = True
+
+        if self.axes_ticks is False:
+            self.ax.set_xticks([])
+            self.ax.set_yticks([])
+
+        self.ax.set_xlabel(self._xlabel)
+        self.ax.set_ylabel(self._ylabel)
 
     def optimize_contrast(self, data):
         if (self._vmin_user is not None and self._vmax_user is not None):
@@ -302,8 +304,9 @@ class ImagePlot(BlittedFigure):
 
     def update(self, **kwargs):
         ims = self.ax.images
-        # update extent:
-        self._compute_extent()
+        # update extent and aspect:
+        self._calculate_extent()
+        self._calculate_aspect()
 
         # Turn on centre_colormap if a diverging colormap is used.
         if self.centre_colormap == "auto":
@@ -363,7 +366,6 @@ class ImagePlot(BlittedFigure):
             self.ax.set_xlim(self._extent[:2])
             self.ax.set_ylim(self._extent[2:])
             ims[0].set_extent(self._extent)
-            self._calculate_aspect()
             self.ax.set_aspect(self._aspect)
             ims[0].norm.vmax, ims[0].norm.vmin = vmax, vmin
             if redraw_colorbar is True:

hyperspy/drawing/signal1d.py
@@ -131,8 +131,8 @@ class Signal1DFigure(BlittedFigure):
                 pass
 
     def set_labels_figure(self):
-        utils.set_signal1d_labels(self.signal, self.ax,
-                                  self._calibrated)
+        utils.set_signal1d_labels(self.signal, self.ax, self._calibrated, 
+                                  ylabel=self.__dict__.get('ylabel'))
 
     def _on_close(self):
         if self.figure is None:
@@ -337,12 +337,9 @@ class Signal1DLine(object):
 
         old_xaxis = self.line.get_xdata()
 
+        xdata = self.axis.axis if calibrated else np.arange(self.axis.high_index + 1)
         if also_xaxis or len(old_xaxis) != self.axis.size or\
                 np.any(np.not_equal(old_xaxis, self.axis.axis)):
-            if calibrated:
-                xdata = self.axis.axis
-            else:
-                xdata = np.arange(self.axis.high_index + 1)
             self.ax.set_xlim(xdata[0], xdata[-1])
             self.line.set_data(xdata, ydata)
         else:
@@ -350,7 +347,7 @@ class Signal1DLine(object):
 
         if self.autoscale is True:
             self.ax.relim()
-            y1, y2 = np.searchsorted(self.axis.axis,
+            y1, y2 = np.searchsorted(xdata,
                                      self.ax.get_xbound())
             y2 += 2
             y1, y2 = np.clip((y1, y2), 0, len(ydata - 1))

hyperspy/signal.py
@@ -1926,8 +1926,11 @@ class BaseSignal(FancySlicing,
             axes_manager = self.axes_manager
         value = np.atleast_1d(
             self.data.__getitem__(axes_manager._getitem_tuple))
+        return self._get_value(value, calibrated=calibrated)
+
+    def _get_value(self, value, calibrated=True):
         gain = self.metadata.get_item("Signal.Noise_properties."
-                                      "Variance_linear_model.gain_factor")
+                              "Variance_linear_model.gain_factor")
         offset = self.metadata.get_item("Signal.Noise_properties."
                                         "Variance_linear_model.gain_offset")
         if gain is not None and offset is not None and calibrated:
@@ -1984,12 +1987,13 @@ class BaseSignal(FancySlicing,
         def get_static_explorer_wrapper(*args, **kwargs):
             return navigator()
 
-        def get_1D_sum_explorer_wrapper(*args, **kwargs):
+        def get_1D_sum_explorer_wrapper(axes_manager=None, calibrated=True):
             navigator = self
             # Sum over all but the first navigation axis.
             am = navigator.axes_manager
             navigator = navigator.sum(am.signal_axes + am.navigation_axes[1:])
-            return np.nan_to_num(navigator.data).squeeze()
+            return self._get_value(np.nan_to_num(navigator.data).squeeze(),
+                                   calibrated=calibrated)
 
         def get_dynamic_explorer_wrapper(*args, **kwargs):
             navigator.axes_manager.indices = self.axes_manager.indices[
@@ -1999,6 +2003,13 @@ class BaseSignal(FancySlicing,
                 return np.abs(navigator())
             else:
                 return navigator()
+
+        def get_image_explorer_wrapper(axes_manager=None, calibrated=True):
+            data = self._get_value(self.data, calibrated=calibrated)
+            if np.issubdtype(self.data.dtype, complex):
+                return np.abs(data)
+            else:
+                return data
 
         if not isinstance(navigator, BaseSignal) and navigator == "auto":
             if (self.axes_manager.navigation_dimension == 1 and
@@ -2051,11 +2062,7 @@ class BaseSignal(FancySlicing,
                         "The navigator dimensions are not compatible with "
                         "those of self.")
             elif navigator == "data":
-                if np.issubdtype(self.data.dtype, complex):
-                    self._plot.navigator_data_function = lambda axes_manager=None: np.abs(
-                        self.data)
-                else:
-                    self._plot.navigator_data_function = lambda axes_manager=None: self.data
+                self._plot.navigator_data_function = get_image_explorer_wrapper
             elif navigator == "spectrum":
                 self._plot.navigator_data_function = get_1D_sum_explorer_wrapper
             else:
@@ -2353,7 +2360,7 @@ class BaseSignal(FancySlicing,
         >>> spectrum.data[1, 2, 9] = 5
         >>> print(spectrum)
         <EDXTEMSpectrum, title: dimensions: (4, 4|10)>
-        >>> print ('Sum = ', sum(sum(sum(spectrum.data))))
+        >>> print('Sum = ', sum(sum(sum(spectrum.data))))
         Sum = 164.0
         >>> scale = [2, 2, 5]
         >>> test = spectrum.rebin(scale)